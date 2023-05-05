fb793a98097efb47c5812d13664825882165871a
Amanda Gamlen <amandagamlen@Amandas-MacBook-Pro.local>
2023-05-05 19:53:26 +1200

    one

---
+++ script.js
@@ -0,0 +1,11 @@
+// function setup() {
+//   createCanvas(windowWidth, windowHeight)
+//   background('white')
+//   colorMode(HSB)
+// }
+// function draw() {
+//   line(mouseX, mouseY, pmouseX, pmouseY)
+// }
+// function mouseClicked() {
+//   strokeWeight(random())
+// }
